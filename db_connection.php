37a85aa49342f58ca8a033c14745feb77eac4d96
Jamison Blackwell <jamie@Jamisons-MacBook-Pro.local>
2021-06-09 00:02:22 -0400

    getContactsById route is working

--- db_connection.php
+++ db_connection.php
@@ -1,6 +1,6 @@
 <?php
 header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Methods: GET");
+header("Access-Control-Allow-Methods: *");
 header("Access-Control-Allow-Headers:  *");
 // header("Access-Control-Allow-Credentials: true");
 // header("Content-Type: application/json; charset=UTF-8");
